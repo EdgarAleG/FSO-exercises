19928cd6aa460e402261d61640170574d9ef68f8
Edgar Ale <edgargonzalezalejandro0@gmail.com>
2024-06-29 10:29:19 -0300

    updating component statuses

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,66 @@ const Display = (props) => {
   )
 }
 
+const History = ({allClicks}) => {
+  if(allClicks.length == 0) {
+    return (
+      <div>This is used for count the clicks</div>
+    )
+  }
+
+  return (
+    <p>Total Clicks: {allClicks.join(' ')}</p>
+  )
+}
+
 const Button = (props) => {
   return (
-    <button onClick={props.onClick} className='button'>
+    <button onClick={props.handleCLick} className='button'>
       {props.title}
     </button>
   )
 }
 
 const App = () => {
-  const [ counter , setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () =>{
-    console.log('increasing, value before', counter)
-     setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const totalClicks = left + 1
+    setLeft(totalClicks)
+    setTotal(totalClicks + right)
   }
-  const reset = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const totalClicks = right + 1
+    setRight(totalClicks)
+    setTotal(totalClicks + left)
   }
-  const decrementByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
+
+  const handleZero = () => {
+    setRight(0)
+    setLeft(0)
+    setAll( [] )
   }
+
   
   return (
     <div className='container'>
 
-      <Display counter={counter} />
+      <p>{left}</p>
+      <Button handleCLick={ handleLeftClick } title={'Left'}/>
 
-      <Button title="+" onClick={increaseByOne} />
+      <Button handleCLick={ handleRightClick } title={'Right'}/>
+      <p>{right}</p>
+      <button onClick={ handleZero }>set to zero</button>
 
-      <Button title="-" onClick={decrementByOne}/>
+      <p>Total de clicks: {total}</p>
 
-      <Button title="reset" onClick={reset}/>
+      <History allClicks={allClicks}/>
     </div>
   )
 }
