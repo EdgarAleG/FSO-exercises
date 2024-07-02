93cc5794f8535ffc44595f051670af0cd60769da
Edgar Ale <edgargonzalezalejandro0@gmail.com>
2024-07-02 20:52:47 -0300

    Finishing part 1d

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = (props) => <div>{props.counter}</div>
 
-const History = ({allClicks}) => {
-  if(allClicks.length == 0) {
-    return (
-      <div>This is used for count the clicks</div>
-    )
-  }
-
-  return (
-    <p>Total Clicks: {allClicks.join(' ')}</p>
-  )
-}
 
 const Button = (props) => {
   return (
@@ -28,45 +13,16 @@ const Button = (props) => {
 }
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const totalClicks = left + 1
-    setLeft(totalClicks)
-    setTotal(totalClicks + right)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const totalClicks = right + 1
-    setRight(totalClicks)
-    setTotal(totalClicks + left)
-  }
-
-  const handleZero = () => {
-    setRight(0)
-    setLeft(0)
-    setAll( [] )
-  }
+  const [value, setValue] = useState(0)
 
   
   return (
     <div className='container'>
 
-      <p>{left}</p>
-      <Button handleCLick={ handleLeftClick } title={'Left'}/>
-
-      <Button handleCLick={ handleRightClick } title={'Right'}/>
-      <p>{right}</p>
-      <button onClick={ handleZero }>set to zero</button>
-
-      <p>Total de clicks: {total}</p>
-
-      <History allClicks={allClicks}/>
+      <Display counter={value}/>
+      <Button handleCLick={ () => setValue(1000)} title={'thousand'}/>
+      <Button handleCLick={ () => setValue(0)} title={'reset'}/>
+      <Button handleCLick={ () => setValue(value + 1)} title={'increase'}/>
     </div>
   )
 }
